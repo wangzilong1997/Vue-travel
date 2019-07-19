862c23bdcdc65e85ce42fd47f223ffd2c4b1b75d
wangzilong <1321787581@qq.com>
2019-07-19 22:03:08 +0800

    主页和城市选择页

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="header">
+      <div class="header-left">
+        <div class="iconfont back-icon">&#xe624;</div>
+      </div>
+      <div class="header-input">
+        <span class="iconfont">&#xe632;</span>输入城市/游玩景点/主题
+      </div>
+      <router-link to='/city'>
+        <div class="header-right">
+          {{this.$store.state.city}}<span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+      </router-link>
+
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '../../../assets/styles/varibles.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+          text-align: center
+          font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      color:#fff
+      width: 1.23rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margn-left:-.04rem
+        font-size: .24rem
+
+</style>
