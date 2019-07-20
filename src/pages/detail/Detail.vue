d638b99d7e073d8e1b9fb4335a1bdb6cb7390950
wangzilong <1321787581@qq.com>
2019-07-20 11:35:05 +0800

    detail-banner

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <detail-banner></detail-banner>
+  </div>
+</template>
+<script>
+import DetailBanner from './components/Banner'
+export default {
+  name: 'Detail',
+  components: {
+    DetailBanner
+  },
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="stylus" scoped>
+</style>
