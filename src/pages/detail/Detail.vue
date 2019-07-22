be712656a7e078fc929a5954491ff449fd0cd984
wangzilong <1321787581@qq.com>
2019-07-22 12:16:10 +0800

    gg

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,24 +1,58 @@
 <template>
   <div class="">
-    <detail-banner></detail-banner>
+    <detail-banner :sightName="sightName"
+                   :bannerImg="bannerImg"
+                   :bannerImgs="gallaryImgs">
+    </detail-banner>
     <detail-header></detail-header>
     <div class="content">
-
+      <detail-list :list="list"></detail-list>
     </div>
   </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   },
-  data: () => ({
-
-  })
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: []
+    }
+  },
+  methods: {
+    getDetailInfo () {
+      console.log(this.$route.params.id)
+      axios.get('/api/detail.json?id=' + this.$route.params.id).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+        console.log(data)
+        console.log(this.sightName)
+        console.log(this.bannerImg)
+        console.log(this.gallaryImgs)
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
+  }
 }
 </script>
 <style lang="stylus" scoped>
--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -27,6 +27,7 @@ export default {
   },
   methods: {
     handleScroll () {
+      console.log('scroll')
       const top = document.documentElement.scrollTop
       if (top > 60) {
         let opacity = top / 140
@@ -41,9 +42,14 @@ export default {
     }
   },
   mounted () {
-    console.log(1)
+    console.log('mounted')
     window.addEventListener('scroll', this.handleScroll)
+  },
+  destroyed () {
+    console.log('destroyed')
+    window.removeEventListener('scroll', this.handleScroll)
   }
+
 }
 </script>
 <style lang="stylus" scoped>
@@ -62,6 +68,7 @@ export default {
       color:#fff
       font-size:.4rem
   .header-fixed
+    z-index: 2
     position:fixed
     top:0
     left:0
